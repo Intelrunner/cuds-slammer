4934d22ea738f2d2fbb6de9992056034f40212e7
Eric <ethridge2004@gmail.com>
2022-05-25 15:32:33 -0400

    Public Repo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cuds-slammer
 AppScript for working with CUD Exports from DoiT Internationals CMP
+
+
+## CUDS Slammer 
+...is a tool to work with CUD Exports from DoiT Internationals CMP. It contains no proprietary software and is free to use and modify in a non-commercial manner. That means you can't use it for generating dubloons for your treasure chest.
+
+## Installation
+Google Script (or .gs) files are simply Javascript files that can be run in Google Apps Script. Take code.js, rename to code.gs and load into your Google sheet as a custom script.
+Instructions: <https://developers.google.com/apps-script/guides/sheets/functions#creating_a_custom_function>
+
+## Warranty and Disclaimer
+None. This is a free software. Use it at your own risk. No support is provided. Feel free to post any issues in Github, I'll get to them as I can (this is a hobby project) and in Alpha.
--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 var ss = SpreadsheetApp.getActiveSpreadsheet();
 
 function findRows() {
-range = SpreadsheetApp.getActiveSheet().getLastRow();
-console.log(range)
-return range;
+  range = SpreadsheetApp.getActiveSheet().getLastRow();
+  console.log(range)
+  return range;
 }
 
 function getSheetName() {
@@ -13,8 +13,8 @@ function getSheetName() {
 }
 
 function renameSheet(newName) {
- // Get a reference to the sheet using its existing name
- // and then rename it using the setName() method.
+  // Get a reference to the sheet using its existing name
+  // and then rename it using the setName() method.
   var sheet = ss.getSheets()[0];
   getSheetName();
   sheet.setName(newName);
@@ -26,9 +26,9 @@ function renameSheet(newName) {
 function createMain() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   ss.insertSheet(0);
-  sheet=ss.getSheets()[1]
+  sheet = ss.getSheets()[1]
   sheet.setName("data")
-  sheet=ss.getSheets()[0]
+  sheet = ss.getSheets()[0]
   sheet.setName("Main")
   return 200
 };
@@ -39,10 +39,10 @@ function insertAverage() {
   sheet.insertColumns(2);
   sheet.getRange("B1").setValue("Average");
   return 200
-  }
+}
 
 function fillAverage() {
-  var data =ss.getSheetByName("data")
+  var data = ss.getSheetByName("data")
   var tgt = ss.getSheetByName("Main").getRange("B2")
 
   tgt.setFormula("=AVERAGE('data'!2:2)")
@@ -54,7 +54,7 @@ function fillAverage() {
 
 }
 
-function copyData() { 
+function copyData() {
   var sheetFrom = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("data");
   var sheetTo = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Main");
 
@@ -62,14 +62,14 @@ function copyData() {
   var valuesToCopy = sheetFrom.getRange(1, 1, sheetFrom.getLastRow(), 1).getValues();
 
   //Paste to another sheet from first cell onwards
-  sheetTo.getRange(1,sheetTo.getLastColumn()+1,valuesToCopy.length,1).setValues   (valuesToCopy);
+  sheetTo.getRange(1, sheetTo.getLastColumn() + 1, valuesToCopy.length, 1).setValues(valuesToCopy);
   return 200
 };
 
-function insertBreakdown(){
+function insertBreakdown() {
   //set sheet
   sheet = ss.getSheetByName("Main")
-  sheet.insertColumns (3,3)
+  sheet.insertColumns(3, 3)
   sheet.getRange("C1").setValue("On Demand")
   sheet.getRange("D1").setValue("1YR CUD")
   sheet.getRange("E1").setValue("3YR CUD")
@@ -83,17 +83,6 @@ function insertBreakdown(){
   tgt.autoFillToNeighbor(SpreadsheetApp.AutoFillSeries.DEFAULT_SERIES)
   //add new column
 }
-
-function insert_od() {
-  sheet = ss.getSheetByName("Main")
-  tgt = sheet.getRange("C2:E2")
-  tgt.autoFillToNeighbor(SpreadsheetApp.AutoFillSeries.DEFAULT_SERIES)
-  Utilities.sleep(1000)
-  return 200
-}
-
-
-
 
 function Main() {
   newName = 'data'
